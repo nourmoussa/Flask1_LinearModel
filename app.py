5fe84e04bd1bdf81de9a797387bf4a50688c847e
nourmoussa <nour-moussa@hotmail.com>
2022-05-31 01:07:09 +0100

    heroku hosting

--- app.py
+++ app.py
@@ -31,10 +31,11 @@
 # if __name__ == '__main__':
 #     app.run()
 
-from flask import Flask
+from flask import Flask, render_template, send_file
 import joblib
 from flask import request
 from interpret import show
+from io import StringIO
 
 app = Flask(__name__)
 model = joblib.load('reg_1.pkl')
@@ -56,8 +57,36 @@ def predict():
     print(type(model))
     lr_local = model.explain_local(data)
     show(lr_local)
-
     return {"result": int(output)}
+
+@app.route('/recieve_data')
+def get_id():
+   the_id = Flask.request.args.get('button_id')
+   return "<p>Got it!</p>"
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+   return Flask.render_template('filename.html')
+
+
+
+
+
+# data = request.get_json(force=True)
+# data = [data["age"], data["gender"], data["height"], data["weight"], data["smoke"], data["alco"], data["active"]]
+# data = [[float(i) for i in data]]
+# lr_local = model.explain_local(data)
+
+# def fig_to_base64(lr_local):
+#     img = io.BytesIO()
+#     lr_local.savefig(img, format='png',
+#                 bbox_inches='tight')
+#     img.seek(0)
+
+#     return base64.b64encode(img.getvalue())
+# encoded = fig_to_base64(lr_local)
+# my_html = '<img src="data:image/png;base64, {}">'.format(encoded.decode('utf-8'))
+
 
 # def explain():
 #     data = request.get_json(force=True)
